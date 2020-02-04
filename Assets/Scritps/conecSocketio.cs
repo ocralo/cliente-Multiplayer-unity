f9003c4f030f8b2571ca5213d4ced6dcc47fb8a1
ocralo <ocralo@gmail.com>
2020-02-04 00:26:24 -0500

    cambios en el unity

--- Assets/Scritps/conecSocketio.cs
+++ Assets/Scritps/conecSocketio.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -6,32 +7,52 @@ using socket.io;
 
 public class conecSocketio : MonoBehaviour
 {
-    public GameObject text;
+    public GameObject text, player;
+    public Socket socket;
+    public string json;
+    public User user;
     // Start is called before the first frame update
     void Start()
     {
         var serverUrl = "http://localhost:7001";
-        var socket = Socket.Connect(serverUrl);
+        socket = Socket.Connect(serverUrl);
 
         // receive "news" event
-        socket.On("news", (string data) =>
+        socket.On("user2", (string data) =>
         {
             Debug.Log(data);
             text.GetComponent<Text>().text = data;
-            /* 
-            // Emit raw string data
-            socket.Emit("my other event", "{ my: data }");
-            */
-
-            // Emit json-formatted string data
-            socket.EmitJson("my other event", @"{ ""my"": ""data"" }");
         });
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (socket.IsConnected)
+        {
+            user.position = player.transform.position;
+            json = JsonUtility.ToJson(user);
+            socket.EmitJson("user"+user.id, json);
+
+            socket.On("user1", (string data) =>
+            {
+                Debug.Log(data);
+                text.GetComponent<Text>().text = data;
+            });
+        }
+        else
+        {
+
+        }
     }
+
+    [Serializable]
+    public class User
+    {
+        public string playerName;
+        public Vector3 position;
+        public int life;
+        public int id;
+    }
+
 }
---
+++ Assets/Scritps/user2Socketio.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using socket.io;
+
+public class user2Socketio : MonoBehaviour
+{
+
+    //public GameObject text;
+    public Socket socket;
+    public User user;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        string serverUrl = "http://localhost:7001";
+        socket = Socket.Connect(serverUrl);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (socket.IsConnected)
+        {
+            socket.On("user2", (string data) =>
+            {
+                //text.GetComponent<Text>().text = data;
+                user = JsonUtility.FromJson<User>(data);
+                this.transform.position = user.position;
+                Debug.Log(user);
+            });
+        }
+    }
+
+
+    [Serializable]
+    public class User
+    {
+        public string playerName;
+        public Vector3 position;
+        public int life;
+    }
+}
