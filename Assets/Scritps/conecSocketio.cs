533191ed6b877424e86f5875c151ba72b17a7646
ocralo <ocralo@gmail.com>
2020-02-11 18:22:16 -0500

    correcciones

--- Assets/Scritps/conecSocketio.cs
+++ Assets/Scritps/conecSocketio.cs
@@ -7,43 +7,78 @@ using socket.io;
 
 public class conecSocketio : MonoBehaviour
 {
-    public GameObject text, player;
+    public GameObject text, player, player2;
     public Socket socket;
     public string json;
     public User user;
+    public User user2;
     // Start is called before the first frame update
     void Start()
     {
         var serverUrl = "http://localhost:7001";
         socket = Socket.Connect(serverUrl);
 
-        // receive "news" event
+        /* // receive "news" event
         socket.On("user2", (string data) =>
         {
             Debug.Log(data);
             text.GetComponent<Text>().text = data;
-        });
+        }); */
+
+    }
+
+    void OnGUI()
+    {
+        Event e = Event.current;
+
+        if (Event.current.Equals(Event.KeyboardEvent(KeyCode.W.ToString())))
+        {
+            Debug.Log("Detected key code: " + e.keyCode);
+            if (socket.IsConnected)
+            {
+                user.position = player.transform.position;
+                json = JsonUtility.ToJson(user);
+                socket.EmitJson("user" + user.id, json);
+                Debug.Log(user2.id);
+                socket.On("user" + user2.id, (string data) =>
+                  {
+                      Debug.Log("entre");
+                      Debug.Log(data.Replace("\"", ""));
+                      text.GetComponent<Text>().text = data;
+                      user2 = JsonUtility.FromJson<User>(data.Replace("\"", "").Replace("'", "\""));
+                      player2.transform.position = user2.position;
+                  });
+            }
+            else
+            {
+
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (socket.IsConnected)
+
+        /* if (socket.IsConnected)
         {
             user.position = player.transform.position;
             json = JsonUtility.ToJson(user);
-            socket.EmitJson("user"+user.id, json);
-
-            socket.On("user1", (string data) =>
-            {
-                Debug.Log(data);
-                text.GetComponent<Text>().text = data;
-            });
+            socket.EmitJson("user" + user.id, json);
+            Debug.Log(user2.id);
+            socket.On("user" + user2.id, (string data) =>
+              {
+                  Debug.Log("entre");
+                  Debug.Log(data.Replace("\"", ""));
+                  text.GetComponent<Text>().text = data;
+                  user2 = JsonUtility.FromJson<User>(data.Replace("\"", "").Replace("'", "\""));
+                  player2.transform.position = user2.position;
+              });
         }
         else
         {
 
-        }
+        } */
     }
 
     [Serializable]
@@ -52,7 +87,15 @@ public class conecSocketio : MonoBehaviour
         public string playerName;
         public Vector3 position;
         public int life;
-        public int id;
+        public float id;
     }
 
+    [Serializable]
+    public class User2
+    {
+        public string playerName;
+        public string position;
+        public string life;
+        public string id;
+    }
 }
